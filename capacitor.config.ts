497e436f7519bc77d06a2ccf33068ade9a6065fb
Eben Krapfl <ekrapfl@opengov.com>
2024-09-30 12:01:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fail-test',
+  webDir: 'www'
+};
+
+export default config;
